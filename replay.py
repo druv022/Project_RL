383e31779ee9fbed3323f84c4d3f2fb82a6855ba
Masoumeh Bakhtiariziabari <31740498+mbakhtiariz@users.noreply.github.com>
2018-12-08 17:01:41 +0100

    cartpole+proportional and ranked based per works

--- replay.py
+++ replay.py
@@ -3,71 +3,14 @@ from collections import deque
 from environment import get_env
 import numpy
 
-class SumTree:
-    # https://github.com/wotmd5731/dqn/blob/master/memory.py
-    write = 0
-
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.tree = numpy.zeros( 2*capacity - 1 )
-        self.data = numpy.zeros( capacity, dtype=object )
-
-    def _propagate(self, idx, change):
-        parent = (idx - 1) // 2
-
-        self.tree[parent] += change
-
-        if parent != 0:
-            self._propagate(parent, change)
-
-    def _retrieve(self, idx, s):
-        left = 2 * idx + 1
-        right = left + 1
-
-        if left >= len(self.tree):
-            return idx
-
-        if s <= self.tree[left]:
-            return self._retrieve(left, s)
-        else:
-            return self._retrieve(right, s-self.tree[left])
-
-    def total(self):
-        return self.tree[0]
-
-    def add(self, p, data):
-        idx = self.write + self.capacity - 1
-
-        self.data[self.write] = data
-        self.update(idx, p)
-
-        self.write += 1
-        if self.write >= self.capacity:
-            self.write = 0
-
-    def update(self, idx, p):
-        change = p - self.tree[idx]
-
-        self.tree[idx] = p
-        self._propagate(idx, change)
-
-    def get(self, s):
-        idx = self._retrieve(0, s)
-        dataIdx = idx - self.capacity + 1
-
-        return (idx, self.tree[idx], self.data[dataIdx])
-
-
-
 
 class NaiveReplayMemory:
-    
+
     def __init__(self, capacity):
         self.capacity = capacity
         self.memory = deque(maxlen=capacity)
 
     def push(self, transition):
-        
         # YOUR CODE HERE
         self.memory.append(transition)
 
@@ -77,22 +20,23 @@ class NaiveReplayMemory:
     def __len__(self):
         return len(self.memory)
 
-#Add different experience replay methods
+
+# Add different experience replay methods
+
 
 class CombinedReplayMemory:
-    
+
     def __init__(self, capacity):
         self.capacity = capacity
         self.memory = deque(maxlen=capacity)
 
     def push(self, transition):
-        
         # YOUR CODE HERE
         self.memory.append(transition)
         self.transition = transition
 
     def sample(self, batch_size):
-        samples = random.sample(self.memory, batch_size-1)
+        samples = random.sample(self.memory, batch_size - 1)
         samples.append(self.transition)
         return samples
 
@@ -100,51 +44,152 @@ class CombinedReplayMemory:
         return len(self.memory)
 
 
-class PrioritizedReplayMemory:   # stored as ( s, a, r, s_ ) in SumTree
-    # modified https://github.com/wotmd5731/dqn/blob/master/memory.py
+class SumTree:
+    # started form https://github.com/wotmd5731/dqn/blob/master/memory.py, and modified considerably.
+    write = 0
 
-    e = 0.01
-    a = 0.6
+    def __init__(self, max_capacity):
+        self.capacity = max_capacity
+        self.tree = numpy.zeros(2 * max_capacity - 1)
+        self.data = numpy.zeros(max_capacity, dtype=object)
+        self.num = 0
+        self.e = 0.01
+        self.a = 0.6
 
-    def __init__(self, capacity):
-        self.tree = SumTree(capacity)
-        self.memory = deque(maxlen=capacity)
-
-    def _getPriority(self, error):
+    def _get_priority(self, error):
         return (error + self.e) ** self.a
 
-    def push(self, error, sample):
-        p = self._getPriority(error)
-        self.tree.add(p, sample)
-        self.memory.append(1)
+    def _propagate(self, idx, change):
+        parent = (idx - 1) // 2
 
-    def sample(self, n):
-        batch_idx = []
-        batch = []
-        segment = self.tree.total() / n
+        self.tree[parent] += change
 
-        for i in range(n):
-            a = segment * i
-            b = segment * (i + 1)
+        if parent != 0:
+            self._propagate(parent, change)
 
-            s = random.uniform(a, b)
-            (idx, p, data) = self.tree.get(s)
-            #print("!!!!!!!!!!!!!!!!",data,i,n)
-            batch.append( data)
-            batch_idx.append(idx)
+    def _retrieve(self, idx, rand):
+        left = 2 * idx + 1
+        right = left + 1
 
-        return batch , batch_idx
+        if left >= len(self.tree):
+            return idx
+
+        if rand <= self.tree[left]:
+            return self._retrieve(left, rand)
+        else:
+            return self._retrieve(right, rand - self.tree[left])
+
+    def _total(self):
+        return self.tree[0]
+
+    def add(self, error, data):
+        p = self._get_priority(error)
+        idx = self.write + self.capacity - 1
+
+        self.data[self.write] = data
+        self.update(idx, p)
+
+        self.write += 1
+        if self.write >= self.capacity:
+            self.write = 0
+        if self.num < self.capacity:
+            self.num += 1
 
     def update(self, idx, error):
-        p = self._getPriority(error)
-        self.tree.update(idx, p)
+        p = self._get_priority(error)
+        change = p - self.tree[idx]
+        self.tree[idx] = p
+        self._propagate(idx, change)
+
+    def _get_single(self):
+        rand = random.uniform(0, self._total())
+        idx = self._retrieve(0, rand)
+        data_idx = idx - self.capacity + 1
+        return idx, self.tree[idx], self.data[data_idx]
+
+    def get_batch(self, n):
+        batch_idx = []
+        batch = []
+        priorities = []
+        for i in range(n):
+            (idx, p, data) = self._get_single()
+            batch.append(data)
+            batch_idx.append(idx)
+            priorities.append(p)
+        return batch, batch_idx, priorities
+
+    def get_len(self):
+        return self.num
+
+
+class RankBased:
+    def __init__(self, max_capacity):
+        self.capacity = max_capacity
+        self.data = deque(maxlen=max_capacity)
+        self.priorities = None
+        self.total = None
+        self.cum_sum = None
+
+    def add(self, error, data):
+        self.data.append(list(data) + [error])
+
+    def update(self, idx, error):
+        self.data[idx][-1] = error
+
+    def _get_single(self):
+        rand = random.uniform(0, self.total)
+        index = numpy.searchsorted(self.cum_sum, rand)
+        return index, self.priorities[index], self.data[index][:-1]  # to exclude the error at the end
+
+    def get_batch(self, n):
+        self._update_priorities()
+        self.total = numpy.sum(self.priorities)
+        self.cum_sum = numpy.cumsum(self.priorities)
+        batch_idx = []
+        batch = []
+        priorities = []
+        for i in range(n):
+            (idx, p, data) = self._get_single()
+            batch.append(data)
+            batch_idx.append(idx)
+            priorities.append(p)
+        return batch, batch_idx, priorities
+
+    def get_len(self):
+        return len(self.data)
+
+    def _update_priorities(self):
+        length = self.get_len()
+        errors = numpy.array([data[-1] for data in self.data])
+        order = numpy.argsort(errors)
+        order = numpy.array([order[order[x]] for x in range(length)])
+        order = length - order
+        self.priorities = 1. / order
+
+
+class PrioritizedReplayMemory:
+    def __init__(self, max_capacity, method="prop"):
+        if method == "prop":
+            self.container = SumTree(max_capacity)
+        elif method == "rank":
+            self.container = RankBased(max_capacity)
+        else:
+            raise ValueError("Bad replay method")
+
+    def push(self, error, sample):
+        self.container.add(error, sample)
+
+    def sample(self, n):
+        return self.container.get_batch(n)
+
+    def update(self, idx, error):
+        self.container.update(idx, error)
 
     def __len__(self):
-        return len(self.memory)
+        return self.container.get_len()
 
 
-#sanity check
-if __name__=="__main__":
+if __name__ == "__main__":
 
     capacity = 10
     memory = PrioritizedReplayMemory(capacity)#CombinedReplayMemory(capacity)#NaiveReplayMemory(capacity)
--- train.py
+++ train.py
@@ -1,106 +1,115 @@
 import numpy as np
 from environment import get_env
 from model import QNetwork
-from replay import* 
+from replay import *
 import argparse
 import torch
 from torch import optim
 import matplotlib.pyplot as plt
 import torch.nn.functional as F
 from tqdm import tqdm as _tqdm
+import random
 from torch.autograd import Variable
 
-#-------setup seed-----------
+# -------setup seed-----------
 random.seed(42)
 torch.manual_seed(42)
 np.random.seed(42)
-#----------------------------
+# ----------------------------
 
 
-#----device-----------
+# ----device-----------
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(device)
-#-------------------
 
+
+# -------------------
 
 
 def tqdm(*args, **kwargs):
-    return _tqdm(*args, **kwargs, mininterval=1)    
+    return _tqdm(*args, **kwargs, mininterval=1)
 
-#using exponential decay rather than linear decay
+
 def get_epsilon(it):
-    
+    # using exponential decay rather than linear decay
     # YOUR CODE HERE
-    return np.exp(-it/450)
+    return np.exp(-it / 450)
+
+
+def get_beta(it, total_it, beta0):
+    return beta0 + (it/total_it) * (1 - beta0)
+
 
 def select_action(model, state, epsilon):
     # YOUR CODE HERE
     state = torch.from_numpy(state).float()
     with torch.no_grad():
         actions = model(state.to(device))
-        
-        rand_num = np.random.uniform(0,1,1)
+
+        rand_num = np.random.uniform(0, 1, 1)
         if epsilon > rand_num:
-            index = torch.randint(0,len(actions),(1,1))
+            index = torch.randint(0, len(actions), (1, 1))
         else:
             value, index = actions.max(0)
-            
+
         return int(index.item())
 
+
 def compute_q_val(model, state, action):
-    
     # YOUR CODE HERE
     actions = model(state)
     return actions.gather(1, action.unsqueeze(1))
+
 
 def compute_target(model, reward, next_state, done, discount_factor):
     # done is a boolean (vector) that indicates if next_state is terminal (episode is done)
     # YOUR CODE HERE
     non_terminal_states_mask = torch.tensor([1 if not s else 0 for s in done])
     non_terminal_states = next_state[non_terminal_states_mask.nonzero().squeeze(1)]
-    
+
     next_state_values = torch.zeros(done.size()[0]).to(device)
-    next_state_values[non_terminal_states_mask.nonzero().squeeze(1)],_ = model(non_terminal_states).max(1)
-    
-    target = reward + discount_factor*next_state_values
-    
+    next_state_values[non_terminal_states_mask.nonzero().squeeze(1)], _ = model(non_terminal_states).max(1)
+
+    target = reward + discount_factor * next_state_values
+
     return target.unsqueeze(1)
 
-def train(model, memory, optimizer, batch_size, discount_factor):
+
+def train(model, memory, optimizer, batch_size, discount_factor, beta):
     # DO NOT MODIFY THIS FUNCTION
-    
+
     # don't learn without some decent experience
     if len(memory) < batch_size:
         return None
 
     # random transition batch is taken from experience replay memory
-    #transitions = memory.sample(batch_size)
-    #---------------------------- per--------------------------
-    transitions, batch_idx = memory.sample(batch_size)
+    # transitions = memory.sample(batch_size)
+    # ---------------------------- per--------------------------
+    transitions, batch_idx, priorities = memory.sample(batch_size)
 
-    if type(transitions[0]) == int:
-        return None
-    #print(batch_idx)
-    # transition is a list of 4-tuples, instead we want 4 vectors (as torch.Tensor's)
+    # transition is a list of 5-tuples, instead we want 5 vectors (as torch.Tensor's)
     state, action, reward, next_state, done = zip(*transitions)
-    
+
     # convert to PyTorch and define types
     state = torch.tensor(state, dtype=torch.float).to(device)
     action = torch.tensor(action, dtype=torch.int64).to(device)  # Need 64 bit to use them as index
     next_state = torch.tensor(next_state, dtype=torch.float).to(device)
     reward = torch.tensor(reward, dtype=torch.float).to(device)
     done = torch.tensor(done, dtype=torch.uint8).to(device)  # Boolean
-    
+
     # compute the q value
     q_val = compute_q_val(model, state, action)
-    
+
     with torch.no_grad():  # Don't compute gradient info for the target (semi-gradient)
         target = compute_target(model, reward, next_state, done, discount_factor)
 
     # loss is measured from error between current and newly expected Q values
-    loss = F.smooth_l1_loss(q_val, target)
-    #loss = F.mse_loss(q_val, target)
-    #------------------------------------------------------ per -------------------------------------
+    w = (1/(batch_size * np.array(priorities)) ** beta)
+    w = torch.tensor(w, dtype=torch.float).to(device).detach()
+
+    loss = torch.mean(w * abs(q_val - target))
+    # loss = F.mse_loss(q_val, target)
+    # ------------------------------------------------------ per -------------------------------------
     td_error = target - q_val
     for i in range(batch_size):
         val = abs(td_error[i].data[0])
@@ -110,27 +119,26 @@ def train(model, memory, optimizer, batch_size, discount_factor):
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
-    
+
     return loss.item()
 
 
 def main():
+    # update this dictionary as per the implementation of methods
+    memory = {'NaiveReplayMemory': NaiveReplayMemory,
+              'CombinedReplayMemory': CombinedReplayMemory,
+              'PrioritizedReplayMemory': PrioritizedReplayMemory}
 
-    #update this disctionary as per the implementation of methods
-    memory= {'NaiveReplayMemory':NaiveReplayMemory,
-             'CombinedReplayMemory' :CombinedReplayMemory,
-             'PrioritizedReplayMemory':PrioritizedReplayMemory}
-
-    #-----------initialization---------------
+    # -----------initialization---------------
     env, (input_size, output_size) = get_env(ARGS.env)
-    replay = memory[ARGS.replay](ARGS.buffer)
+    replay = memory[ARGS.replay](ARGS.buffer, ARGS.pmethod)
 
-    model =  QNetwork(input_size, output_size, ARGS.num_hidden).to(device)
+    model = QNetwork(input_size, output_size, ARGS.num_hidden).to(device)
     optimizer = optim.Adam(model.parameters(), ARGS.lr)
 
     global_steps = 0  # Count the steps (do not reset at episode start, to compute epsilon)
     episode_durations = []  #
-    #-------------------------------------------------------
+    # -------------------------------------------------------
 
     for i in tqdm(range(ARGS.num_episodes)):
         # YOUR CODE HERE
@@ -139,73 +147,67 @@ def main():
         done = False
         epi_duration = 0
         while not done:
-            td_err=0
             eps = get_epsilon(global_steps)
             a = select_action(model, s, eps)
             s_next, r, done, _ = env.step(a)
 
-            q_val= model(torch.tensor(s, dtype=torch.float).to(device))[a]#.unsqueeze(1)
-            #print(act, act.unsqueeze(1))
-            target = (torch.tensor(r, dtype=torch.float).to(device) +\
-                 ARGS.discount_factor*torch.tensor(s_next, dtype=torch.float).to(device)).max()
-            #print(q_val)
-            #print(target)
-            td_err = target-q_val
-            #print(td_err)
+            q_val = model(torch.tensor(s, dtype=torch.float).to(device))[a]
+            target = torch.tensor(r, dtype=torch.float).to(device) + \
+                     ARGS.discount_factor * model(torch.tensor(s_next, dtype=torch.float).to(device)).max()
+            # print(q_val)
+            # print(target)
+            #td_err = target - q_val
+            td_err = F.smooth_l1_loss(q_val, target.detach())
+            # print(td_err)
+            # print(torch.isnan(td_err))
+
             replay.push(td_err.detach(), [s, a, r, s_next, done])
 
-            #replay.push((s, a, r, s_next, done))
-            loss = train(model, replay, optimizer, ARGS.batch_size, ARGS.discount_factor)
+            # replay.push((s, a, r, s_next, done))
+            beta = get_beta(i, ARGS.num_episodes, ARGS.beta0)
+            train(model, replay, optimizer, ARGS.batch_size, ARGS.discount_factor, beta)
 
             s = s_next
             epi_duration += 1
-            global_steps +=1
+            global_steps += 1
+            #if epi_duration >= 100:
+            #    done = True
 
         episode_durations.append(epi_duration)
 
     #
-    cumsum = np.cumsum(np.insert(episode_durations, 0, 0)) 
-    cumsum = (cumsum[10:] - cumsum[:-10]) / float(10)
+    cum_sum = np.cumsum(np.insert(episode_durations, 0, 0))
+    cum_sum = (cum_sum[10:] - cum_sum[:-10]) / float(10)
 
-    plt.plot(cumsum)
+    plt.plot(cum_sum)
     plt.title('Episode durations per episode')
-    plt.show()    
+    plt.show()
     return episode_durations
 
 
-
-
-
-
-
-
 if __name__ == "__main__":
-
-    
     parser = argparse.ArgumentParser()
     parser.add_argument('--num_episodes', default=100, type=int,
                         help='max number of episodes')
     parser.add_argument('--batch_size', default=64, type=int)
-                      
+
     parser.add_argument('--num_hidden', default=128, type=int,
                         help='dimensionality of hidden space')
     parser.add_argument('--lr', default=1e-3, type=float)
     parser.add_argument('--discount_factor', default=0.8, type=float)
-    #parser.add_argument('--replay', default='NaiveReplayMemory',type=str,
+    # parser.add_argument('--replay', default='NaiveReplayMemory',type=str,
     #                    help='type of experience replay')
 
-    parser.add_argument('--replay', default='PrioritizedReplayMemory',type=str,
+    parser.add_argument('--replay', default='PrioritizedReplayMemory', type=str,
                         help='type of experience replay')
 
-    #parser.add_argument('--replay', default='CombinedReplayMemory', type=str,
-     #                   help='type of experience replay')
+    # parser.add_argument('--replay', default='CombinedReplayMemory', type=str,
+    #                   help='type of experience replay')
     parser.add_argument('--env', default='CartPole-v1', type=str,
                         help='environments you want to evaluate')
     parser.add_argument('--buffer', default='100000', type=int,
                         help='buffer size for experience replay')
-
+    parser.add_argument('--beta0', default=0.4, type=float)
+    parser.add_argument('--pmethod', default='prop', type=str, help='proritized reply method: {prop or rank}')
     ARGS = parser.parse_args()
-
     main()
-
-
