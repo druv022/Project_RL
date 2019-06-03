f943a814a580ae0dd1857659c352fc07bee372e7
TARUNK <tarunk@pop-os.localdomain>
2019-06-03 17:23:47 +0200

    merged in code

--- jobs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-for x in {1..10}
-do
-    python train.py --num_episodes 1000 --batch_size 64 --num_hidden 64 --lr 5e-4 --discount_factor 0.99 --replay PER --env LunarLander-v2 --buffer 10000 --pmethod prop --TAU 0.1
-done
